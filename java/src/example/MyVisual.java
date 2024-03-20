4c65da588c8d19a7b8a4b12123181d2fe7186722
Wissam Hadjarab <C21404706@mytudublin.ie>
2024-03-20 14:23:28 +0000

    added first version of song

--- java/src/example/MyVisual.java
+++ java/src/example/MyVisual.java
@@ -20,7 +20,7 @@ public class MyVisual extends Visual {
         startMinim();
 
         // Call loadAudio to load an audio file to process
-         loadAudio("heroplanet.mp3");
+         loadAudio("song.mp3");
 
         // Call this instead to read audio from the microphone
         //startListening();
