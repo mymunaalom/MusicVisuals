81d70986389204904a8e36c8cff4ed37f3c26477
iriaPM <c22305863@mytudublin.ie>
2024-04-10 22:30:24 +0100

    coordinatewithmusic

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,6 +1,7 @@
 package ie.tudublin;
 
 import c22305863.iriasVisual;
+import c22305863.iriasvis2;
 import example.CubeVisual;
 import example.MyVisual;
 import example.RotatingAudioBands;
@@ -9,7 +10,7 @@ public class Main {
 
     public void startUI() {
         String[] a = { "MAIN" };
-        processing.core.PApplet.runSketch(a, new iriasVisual());
+        processing.core.PApplet.runSketch(a, new iriasvis2());
     }
 
     public static void main(String[] args) {
