76c8328e0948a9fbfa1f468e93790a72340cddfa
iriaPM <c22305863@mytudublin.ie>
2024-04-24 16:51:51 +0100

    fix

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,11 +1,7 @@
 package ie.tudublin;
 
 import c22305863.iriaVisual;
-<<<<<<< HEAD
-=======
 import c21404706.wissamVisual;
-import c22305863.IriaVisual2;
->>>>>>> 249e807053a4ce8badcd13d4583b23754f30830e
 import example.CubeVisual;
 import example.MyVisual;
 import example.RotatingAudioBands;
@@ -14,11 +10,7 @@ public class Main {
 
     public void startUI() {
         String[] a = { "MAIN" };
-<<<<<<< HEAD
         processing.core.PApplet.runSketch(a, new MyVisual2());
-=======
-        processing.core.PApplet.runSketch(a, new wissamVisual());
->>>>>>> 249e807053a4ce8badcd13d4583b23754f30830e
     }
 
     public static void main(String[] args) {
