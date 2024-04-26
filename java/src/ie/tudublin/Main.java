f9f13040ab08e77538b0105e63f6e5ea06ed94eb
iriaPM <c22305863@mytudublin.ie>
2024-04-26 13:55:44 +0100

    finalyyyy

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -3,12 +3,13 @@ package ie.tudublin;
 import c22305863.iriaVisual;
 import example.CubeVisual;
 import example.RotatingAudioBands;
+import ie.tudublin.Allfiles;
 
 public class Main {
 
     public void startUI() {
         String[] a = { "MAIN" };
-        processing.core.PApplet.runSketch(a, new iriaVisual());
+        processing.core.PApplet.runSketch(a, new Allfiles());
     }
 
     public static void main(String[] args) {
